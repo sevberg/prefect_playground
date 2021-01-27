34105c9d227e18f9bfe5dc8cf11712fa6cfe9cf9
sevberg <s.ryberg@accure.net>
2021-01-27 12:47:15 +0100

    add simple task

---
+++ prefectplayground/tasks.py
@@ -0,0 +1,18 @@
+from prefect import task
+import numpy as np
+from typing import List, Sized
+
+
+@task(log_stdout=True)
+def generate_list(
+    n_members: int = 100, min_value: int = 1000, max_value: int = 2000, cycles: int = 1
+) -> List[int]:
+    """Generate a list of length `n_members` between `min_value` and `max_value`.
+
+    `cycles` can be used to increase runtime"""
+
+    for _ in range(cycles):
+        output = np.random.choice(np.arange(min_value, max_value + 1), size=n_members)
+
+    print(output)
+    return output.tolist()
