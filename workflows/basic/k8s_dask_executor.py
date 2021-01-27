9b99b096dc7c03bf7f029d2d80e6bfae7a8af0a2
sevberg <s.ryberg@accure.net>
2021-01-27 19:15:35 +0100

    try stuff

--- workflows/basic/k8s_dask_executor.py
+++ workflows/basic/k8s_dask_executor.py
@@ -1,8 +1,11 @@
+import prefect
 from prefect.storage import Docker
-from prefect.run_configs import KubernetesRun
+from prefect.run_configs import ECSRun
 from prefect.executors.dask import DaskExecutor
 from os import path
 from datetime import datetime
+
+# from dask_cloudprovider.aws import FargateCluster
 
 import flow_generator
 
@@ -16,6 +19,9 @@ module_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 flow.storage = Docker(
     python_dependencies=[
         "numpy",
+        "dask_kubernetes==0.11.0",
+        "dask==2020.12.0",
+        # "dask-cloudprovider[aws]",
     ],
     registry_url="public.ecr.aws/m5p2l3e5/",
     image_name="prefect_playground",
@@ -35,11 +41,63 @@ flow.storage = Docker(
     extra_dockerfile_commands=["RUN pip install -e /modules/prefect_playground"],
 )
 
-flow.run_config = KubernetesRun(
-    cpu_request=2, memory_request="2Gi", env={"SOME_VAR": "value"}
+flow.run_config = ECSRun(
+    task_role_arn="arn:aws:iam::495775544086:role/ECS-task-execution-role",
+    image="prefecthq/prefect:0.14.5",
+    memory="2048",
+    cpu="1024",
 )
 
+# flow.run_config = KubernetesRun(
+#     cpu_request=2, memory_request="2Gi", env={"AWS_DEFAULT_REGION": "eu-central-1"}
+# )
 
-flow.executor = DaskExecutor(cluster_kwargs={"n_workers": 5, "threads_per_worker": 1})
+# flow.run_config = prefect.run_configs.L
+
+# flow.run_config = KubernetesRun(
+#     job_template_path="my_custom_template.yaml"
+# )
+
+
+# def make_cluster(n_workers, image):
+#     """Start a fargate cluster using the same image as the flow run"""
+#     from dask_kubernetes import KubeCluster, make_pod_spec
+
+#     pod_spec = make_pod_spec(
+#         # image=image,
+#         image = 'daskdev/dask:latest',
+#         memory_limit="3G",
+#         memory_request="3G",
+#         cpu_limit=1,
+#         cpu_request=1,
+#         # env={'EXTRA_PIP_PACKAGES': 'fastparquet git+https://github.com/dask/distributed'}
+#     )
+
+#     return KubeCluster(pod_spec, n_workers=n_workers)
+
+
+def make_cluster(n_workers, image):
+    """Start a fargate cluster using the same image as the flow run"""
+
+    # return FargateCluster(
+    #     n_workers=n_workers, image=image, region_name="eu-central-1"
+    # )  # prefect.context.image)
+    from dask_cloudprovider.aws import ECSCluster
+
+    cluster = ECSCluster(
+        run_task_kwargs={
+            "cluster": "accure_compute_2xlarge_Batch_5e1ca55c-c98e-36f3-a199-d76f2f96caa4",
+        },
+        cluster_arn="arn:aws:ecs:eu-central-1:495775544086:cluster/accure_compute_2xlarge_Batch_5e1ca55c-c98e-36f3-a199-d76f2f96caa4",
+        image=image,
+        n_workers=n_workers,
+    )
+
+
+flow.executor = DaskExecutor(
+    # cluster_class=make_cluster,
+    # cluster_kwargs={"n_workers": 60, "image": flow.storage.name},
+)
 
 flow.register(project_name="prefect_playground", labels=["dev"])
+# flow.run()
